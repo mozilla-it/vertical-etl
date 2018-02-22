6dc09e6f59aab6c2e7c94d3b27279250ff456952
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-22 11:06:48 -0500

    pp-lint

--- nubis/puppet/packages.pp
+++ nubis/puppet/packages.pp
@@ -1,12 +1,12 @@
-$vsql_major_version = "8.1"
+$vsql_major_version = '8.1'
 $vsql_version = "${vsql_major_version}.1-13"
 
 package { 'vsql':
-  provider => 'rpm',
-  ensure => present,
-  name => 'vertica-client-fips'
-  source => "https://my.vertica.com/client_drivers/${vsql_major_version}.x/${vsql_version}/vertica-client-fips-${vsql_version}.${::architecture}.rpm",
+  ensure          => present,
+  provider        => 'rpm',
+  name            => 'vertica-client-fips',
+  source          => "https://my.vertica.com/client_drivers/${vsql_major_version}.x/${vsql_version}/vertica-client-fips-${vsql_version}.${::architecture}.rpm",
   install_options => [
-    "--noscripts",
+    '--noscripts',
   ],
 }

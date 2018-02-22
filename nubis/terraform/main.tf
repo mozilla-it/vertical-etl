0c952b7dfabed2a33dac41d3955ddd0a447f6621
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-22 10:21:45 -0500

    no ELB

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -6,7 +6,6 @@ module "worker" {
   service_name      = "${var.service_name}"
   purpose           = "etl"
   ami               = "${var.ami}"
-  elb               = "${module.load_balancer.name}"
   ssh_key_file      = "${var.ssh_key_file}"
   ssh_key_name      = "${var.ssh_key_name}"
   nubis_sudo_groups = "${var.nubis_sudo_groups}"

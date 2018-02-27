a1c1ba3edebc177ee366d566712a37568f607cc2
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-27 13:04:27 -0500

    add missing Python build dependencies

--- nubis/puppet/python.pp
+++ nubis/puppet/python.pp
@@ -4,8 +4,20 @@ class { 'python':
   dev     => true,
 }
 
+package { 'gcc-c++':
+  ensure => 'present',
+}
+
+package { 'unixODBC-devel':
+  ensure => 'present',
+}
+
 # Install Mozilla's data-collector
 python::pip { 'data-collectors':
-  ensure => 'present',
-  url    => 'git+https://github.com/mozilla-it/data-collectors',
+  ensure  => 'present',
+  url     => 'git+https://github.com/mozilla-it/data-collectors',
+  require => [
+    Package['gcc-c++'],
+    Package['unixODBC-devel'],
+  ],
 }

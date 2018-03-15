501acd3238413eca8d957a94f45a796ffa3a118e
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-15 13:01:10 -0400

    add workday code

---
+++ nubis/puppet/workday.pp
@@ -0,0 +1,25 @@
+file { '/opt/workday':
+  ensure => directory,
+}
+
+file { "/opt/workday/fetch":
+  ensure => present,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  require => [
+    File ['/opt/workday'],
+  ],
+  source => 'puppet:///nubis/files/workday/fetch_workday_data.py',
+}
+
+file { "/opt/workday/fetch-plus":
+  ensure => present,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  require => [
+    File ['/opt/workday'],
+  ],
+  source => 'puppet:///nubis/files/workday/fetch_workday_data_plus.py',
+}

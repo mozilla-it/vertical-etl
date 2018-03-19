f8fa9e5ea2eb7e8917fb4a08242ecd68d8e14c31
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-19 12:59:22 -0400

    puppet lint

--- nubis/puppet/workday.pp
+++ nubis/puppet/workday.pp
@@ -29,24 +29,24 @@ file { '/var/lib/etl/workday':
   ]
 }
 
-file { "/opt/workday/fetch":
-  ensure => present,
-  owner  => root,
-  group  => root,
-  mode   => '0755',
+file { '/opt/workday/fetch':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
   require => [
     File['/opt/workday'],
   ],
-  source => 'puppet:///nubis/files/workday/fetch_workday_data.py',
+  source  => 'puppet:///nubis/files/workday/fetch_workday_data.py',
 }
 
-file { "/opt/workday/fetch-plus":
-  ensure => present,
-  owner  => root,
-  group  => root,
-  mode   => '0755',
+file { '/opt/workday/fetch-plus':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
   require => [
     File['/opt/workday'],
   ],
-  source => 'puppet:///nubis/files/workday/fetch_workday_data_plus.py',
+  source  => 'puppet:///nubis/files/workday/fetch_workday_data_plus.py',
 }

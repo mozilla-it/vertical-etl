a708e4d3d2f59ac9dc0dfc7c3f86b1b047ffead7
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-21 13:47:16 -0400

    add Bugzilla ES import job

---
+++ nubis/puppet/bugzilla.pp
@@ -0,0 +1,37 @@
+cron::daily { "${project_name}-fetch-bugzilla-es":
+  hour    => '1',
+  minute  => fqdn_rand(60),
+  user    => 'etl',
+  command => "nubis-cron ${project_name}-fetch-bugzilla-es /opt/etl/bugzilla/run",
+}
+
+file { '/opt/etl/bugzilla':
+  ensure => directory,
+  require => [
+    File['/opt/etl'],
+  ]
+}
+
+file { '/var/lib/etl/bugzilla':
+  ensure  => directory,
+  owner   => 'etl',
+  group   => 'etl',
+  mode    => '0755',
+
+  require => [
+    User['etl'],
+    Group['etl'],
+    File['/var/lib/etl'],
+  ]
+}
+
+file { '/opt/etl/bugzilla/fetch':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  require => [
+    File['/opt/etl/bugzilla'],
+  ],
+  source  => 'puppet:///nubis/files/bugzilla/fetch.py',
+}

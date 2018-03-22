0210531392a4cca22d2d58a72afb2ba96b6a8550
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-03-22 09:52:02 -0400

    Python lint

--- nubis/files/search/load-daily.py
+++ nubis/files/search/load-daily.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python
 
-import os
-
-from datetime import timedelta
-import datetime
-import sys
-import pyodbc
 from optparse import OptionParser
-from datetime import datetime, timedelta
+import pyodbc
 
 cnxn = pyodbc.connect("DSN=vertica")
 cursor = cnxn.cursor()
 
 parser = OptionParser()
 parser.add_option('-f', '--file', dest='log_file', help='Log file to process', type='string')
-parser.add_option('-d', '--date', dest='process_date', help='Date to process', type='string')
+
 (options, args) = parser.parse_args()
 
-if options.process_date is None:
-    today = datetime.today()
-    Date = today.strftime('%Y-%m-%d')
-
-search_data_file=options.log_file
+search_data_file = options.log_file
 
 copy_sql = """
     COPY v4_submissionwise_v5 FROM LOCAL '%s' GZIP DELIMITER '|' DIRECT;

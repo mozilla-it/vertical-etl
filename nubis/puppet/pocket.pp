e6da76987239e42968b8a169fa32e6d6f8753fe6
nawong <nawong@mozilla.com>
2018-04-10 09:11:24 -0700

    Add puppet file for Pocket DAU cron job

---
+++ nubis/puppet/pocket.pp
@@ -0,0 +1,63 @@
+cron::daily { "${project_name}-pocket":
+  hour    => '10',
+  minute  => fqdn_rand(60),
+  user    => 'etl',
+  command => "nubis-cron ${project_name}-pocket /opt/etl/pocket/run",
+}
+
+package { 'pyodbc':
+  ensure => present
+}
+
+file { '/opt/etl/pocket':
+  ensure  => directory,
+  require => [
+    File['/opt/etl'],
+  ]
+}
+
+file { '/var/lib/etl/pocket':
+  ensure  => directory,
+  owner   => 'etl',
+  group   => 'etl',
+  mode    => '0755',
+
+  require => [
+    User['etl'],
+    Group['etl'],
+    File['/var/lib/etl'],
+  ]
+}
+
+file { '/opt/etl/pocket/fetch':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  require => [
+    File['/opt/etl/pocket'],
+  ],
+  source  => 'puppet:///nubis/files/pocket/fetch',
+}
+
+file { '/opt/etl/pocket/load':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  require => [
+    File['/opt/etl/pocket'],
+  ],
+  source  => 'puppet:///nubis/files/pocket/load',
+}
+
+file { '/opt/etl/pocket/run':
+  ensure  => present,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  require => [
+    File['/opt/etl/pocket'],
+  ],
+  source  => 'puppet:///nubis/files/pocket/run',
+}

2f6da14a59f47476e7763dd75c3db33b356124e1
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-04-11 09:15:07 -0400

    pylint cleanups

--- nubis/files/pocket/load.py
+++ nubis/files/pocket/load.py
@@ -8,13 +8,8 @@ import os
 import os.path
 import logging
 import logging.config
-import time
 import glob
-from datetime import date, timedelta, datetime
-from collections import namedtuple
-import urlparse
 import pyodbc
-
 
 logger = logging.getLogger(__name__)
 
@@ -23,58 +18,52 @@ cnxn = pyodbc.connect("DSN=vertica", autocommit=False)
 def transform(fn_in, fn_out):
     logger.debug("fileout: " + fn_out)
     lines = []
-    fileout = open(fn_out,'w')
+    fileout = open(fn_out, 'w')
     for line in open(fn_in):
         parts = line.split('\t')
         parts[7] = 'Pocket'
         lines.append('\t'.join(parts))
     fileout.write('\n'.join(lines))
-    fileout.close()    
+    fileout.close()
 
-def load_into_vertica(input_file, tbl_name, delimiter = '\x01', field_order = []):
-  cursor = cnxn.cursor()
 
-  ordered_fields = ""
-  if field_order:
-    ordered_fields = '(%s)' % (','.join(field_order))
+def load_into_vertica(input_file, tbl_name, delimiter='\x01', field_order=None):
+    cursor = cnxn.cursor()
 
-  sql_tmpl = ("COPY {table_name} {ordered_fields} FROM LOCAL '{local_path}' "
-              " delimiter '{delimiter}' EXCEPTIONS 'exceptions.txt' "
-              " REJECTED DATA 'rejected.txt' NO COMMIT;")
+    ordered_fields = ""
+    if field_order:
+        ordered_fields = '(%s)' % (','.join(field_order))
 
-  sql = sql_tmpl.format(table_name = tbl_name,
-                        ordered_fields = ordered_fields,
-                        local_path = input_file,
-                        delimiter = delimiter)
+    sql_tmpl = ("COPY {table_name} {ordered_fields} FROM LOCAL '{local_path}' "
+                " delimiter '{delimiter}' EXCEPTIONS 'exceptions.txt' "
+                " REJECTED DATA 'rejected.txt' NO COMMIT;")
 
-  logger.debug("importing data into vertica: " + sql)
-  cursor.execute(sql)
+    sql = sql_tmpl.format(table_name=tbl_name,
+                          ordered_fields=ordered_fields,
+                          local_path=input_file,
+                          delimiter=delimiter)
 
-  # TODO refactor vertica helpers, define custom exceptions
-  if os.path.exists('rejected.txt') and os.path.getsize('rejected.txt') != 0:
-    logger.debug, 'rejected data found during load_into_vertica'
+    logger.debug("importing data into vertica: " + sql)
+    cursor.execute(sql)
+
+    # TODO refactor vertica helpers, define custom exceptions
+    if os.path.exists('rejected.txt') and os.path.getsize('rejected.txt') != 0:
+        logger.debug('rejected data found during load_into_vertica')
+
 
 def query_vertica(query):
-  cursor = cnxn.cursor()
-  cursor.execute(query)
-
-  def it():
-    for i in cursor:
-      yield i
-
-  # return cursor itself ?
-  return it()
-
-
+    cursor = cnxn.cursor()
+    cursor.execute(query)
 
 
 def main():
     input_dir = '/var/lib/etl/pocket/'
     # get the most recent file in input_dir
-    input_fn = max(glob.glob(input_dir + 'mobile_active_counts_*'), key=os.path.getctime)
+    input_fn = max(
+        glob.glob(input_dir + 'mobile_active_counts_*'), key=os.path.getctime)
     if not input_fn:
         logger.error('No S3 file found in ' + input_dir)
-        raise IOError, 'Missing file error'
+        raise IOError('Missing file error')
 
     vertica_table_name = 'pocket_mobile_daily_active_users'
 
@@ -87,29 +76,33 @@ def main():
     query_vertica("TRUNCATE TABLE " + vertica_table_name + ";")
 
     logger.debug('loading data into Vertica')
-    field_order = ['activity_date','platform','dau','wau_rolling_7','mau_rolling_30','mau_rolling_31','mau_rolling_28','app']
-    load_into_vertica(output_fn,vertica_table_name,delimiter='\t',field_order = field_order)
+    field_order = ['activity_date', 'platform', 'dau', 'wau_rolling_7',
+                   'mau_rolling_30', 'mau_rolling_31', 'mau_rolling_28', 'app']
+    load_into_vertica(output_fn, vertica_table_name,
+                      delimiter='\t', field_order=field_order)
 
     # update last_updated with result
     logger.debug('inserting completed load in last_updated for monitoring')
-    last_updated_sql = ("insert into last_updated values ('" + vertica_table_name + "', now(), 'Cron-Loader');")
+    last_updated_sql = ("insert into last_updated values ('" +
+                        vertica_table_name + "', now(), '" + __file__ + "')")
     query_vertica(last_updated_sql)
 
     # All worked, commit it all
     logger.debug('final transaction commit')
     cnxn.commit()
 
+
 if __name__ == '__main__':
 
-  try:
-    logging.basicConfig(format = '%(asctime)s %(name)s:%(levelname)s: %(message)s',
-                      level = logging.DEBUG)
+    try:
+        logging.basicConfig(format='%(asctime)s %(name)s:%(levelname)s: %(message)s',
+                            level=logging.DEBUG)
 
-    main()
+        main()
 
-  except:
-    logging.basicConfig(format = '%(asctime)s %(name)s:%(levelname)s: %(message)s',
-                        level = logging.DEBUG)
-    logger.exception("error in running ETL")
+    except:
+        logging.basicConfig(format='%(asctime)s %(name)s:%(levelname)s: %(message)s',
+                            level=logging.DEBUG)
+        logger.exception("error in running ETL")
 
-    sys.exit(1)
+        sys.exit(1)

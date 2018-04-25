9760affd1c7e795dbe22e9e697df762047ef8a38
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-04-25 14:21:52 -0400

    shellcheck cleanup

--- nubis/files/churn/call_load.sh
+++ nubis/files/churn/call_load.sh
@@ -2,14 +2,11 @@
 
 FETCH_DATE=$(date +%Y-%m-%d) # default is to process for current date
 
-APP_DIR=search-cohort-churn/
-
 XFER_FILE_DIR=/var/lib/etl/chur/latest/work
 set -e
 
-for file in $XFER_FILE_DIR/*.csv.gz
+for file in "$XFER_FILE_DIR"/*.csv.gz
 do
-	echo $file
 	# pass file to python script to load
-	/opt/etl/churn/load -f $file -d $FETCH_DATE
+	/opt/etl/churn/load -f "$file" -d "$FETCH_DATE"
 done

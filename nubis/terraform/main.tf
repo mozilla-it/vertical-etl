8e41c79052f3426faf4640ab35d08369520941a5
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-01 14:24:30 -0400

    Enable S3 lifecycle policies for backups
    Fixes #6

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -37,8 +37,9 @@ module "nagios" {
 
 #XXX: region fix will be released after Nubis v2.2.0 (3fb2ebe3023e18df01667e1c9b43503c0f09bf3c)
 #XXX: SSE feature will be released after Nubis v2.2.0 (9ad6e588cc7db80a53725e6a0ea62a8384383d6d)
+#XXX: Expiration policies will be released after Nubis v2.2.0 (gozer:da652f880800981e123b2f5621ca3d0c50a5a773)
 module "backups" {
-  source                    = "github.com/nubisproject/nubis-terraform//bucket?ref=9ad6e588cc7db80a53725e6a0ea62a8384383d6d"
+  source                    = "github.com/gozer/nubis-terraform//bucket?ref=da652f880800981e123b2f5621ca3d0c50a5a773"
   region                    = "${var.backup_region}"
   environment               = "${var.environment}"
   account                   = "${var.account}"
@@ -46,4 +47,10 @@ module "backups" {
   purpose                   = "backups"
   role                      = "${module.worker.role}"
   storage_encrypted_at_rest = true
+  expiration_days           = 930
+
+  transitions = {
+    GLACIER     = 186
+    STANDARD_IA = 60
+  }
 }

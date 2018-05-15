feb4233464cfea17ef376df60f464be04aa47418
nawong <nawong@mozilla.com>
2018-05-15 06:25:55 -0700

    Update load for nagios logs

--- nubis/files/nagios/load.py
+++ nubis/files/nagios/load.py
@@ -1,26 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
-"""S3 to Vertica ETL : BLP ADI Counts
+"""S3 to Vertica ETL : Nagios Logs
 """
 
 import sys
 import os
 import os.path
-import logging
-import logging.config
 import time
-from datetime import date, timedelta, datetime
-from collections import namedtuple
 import urlparse
 import pyodbc
+import logging
+import logging.config
+from optparse import OptionParser
+from subprocess import Popen, PIPE
 
 logger = logging.getLogger(__name__)
 
+# The default nagios/datacenters that run if no args are provided.
+nagios_dc = ['nagios3.private.scl3.mozilla.com', 'nagios1.private.corp.phx1.mozilla.com', 'nagios1.private.scl3.mozilla.com', 'nagios1.private.phx1.mozilla.com', 'nagios2.private.scl3.mozilla.com']
+# Used inconjunction with default args
+default_path = '/var/lib/etl/nagios'
+
 cnxn = pyodbc.connect("DSN=vertica", autocommit=False)
 
-DELETE_QUERY_TMPL = """
-DELETE FROM {table_name} WHERE (bl_date >= '{start_date}') AND (bl_date <= '{end_date}');
-"""
+
+
+def query_vertica(query):
+  cursor = cnxn.cursor()
+  cursor.execute(query)
+
+  def it():
+    for i in cursor:
+      yield i
+
+  return it()
+
 
 def load_into_vertica(input_file, tbl_name, delimiter = '\x01', field_order = []):
   cursor = cnxn.cursor()
@@ -45,75 +59,64 @@ def load_into_vertica(input_file, tbl_name, delimiter = '\x01', field_order = []
   if os.path.exists('rejected.txt') and os.path.getsize('rejected.txt') != 0:
     logger.debug, 'rejected data found during load_into_vertica'
 
-def query_vertica(query):
-  cursor = cnxn.cursor()
-  cursor.execute(query)
 
-  def it():
-    for i in cursor:
-      yield i
+def ProcessIt(fullfn,fn):
+    transform_output_file = "nagios-parse-output.txt"
+    data = open(transform_output_file, 'w')
 
-  # return cursor itself ?
-  return it()
+    field_order = [ 'event_occurred_at', 'incident_type', 'host', 'service', 'status', 'notify_by', 'description', 'filename' ]
+    f = Popen(['zcat', fullfn], stdout=PIPE)
+    line_count=0
+    for i in f.stdout:
+        fields = i.rstrip().split(';', 6)
 
+        hdr_fields = fields[0].split()
 
-def read_meta(input_file):
-  row_count = 0L
+        utc_time = time.gmtime(float(hdr_fields[0][1:-1]))
+        notification_type = ' '.join(hdr_fields[1:-1])
+        notification_target = hdr_fields[-1]
 
-  for line in open(input_file, 'r'):
-    row_count += 1
+        if not notification_type.endswith(' NOTIFICATION:'):
+            continue
 
-  return {'row_count' : row_count}
+        try:
+            alert_host = fields[1]
+        except (IndexError):
+            continue 
 
-def main(start_date, end_date):
-    input_file = '/var/lib/etl/adi/' + start_date.strftime('%Y-%m-%d') + '/output'
-    vertica_table_name = 'copy_adi_dimensional_by_date_s3'
+        if notification_type.startswith('HOST'):
+            alert_name = ""
+            status = fields[2]
+            notify_by = fields[3]
+            rest = fields[4:]
+        else:
+            alert_name = fields[2]
+            status = fields[3]
+            notify_by = fields[4]
+            rest = fields[5:]
 
-    # process/transform/aggregate input data
-    logger.debug('transforming data')
-    meta = read_meta(input_file)
+        fmt_time = time.strftime('%Y-%m-%d %H:%M:%S', utc_time)
 
-    # load data into vertica
-    logger.debug('removing existing data')
-    query_vertica(DELETE_QUERY_TMPL.format(
-      table_name = vertica_table_name,
-      start_date = start_date.strftime("%Y-%m-%d"),
-      end_date = end_date.strftime("%Y-%m-%d")))
+        payload = chr(1).join([fmt_time, #hdr_fields[0][1:-1],
+                     '%s %s' % (notification_type, notification_target),
+                     alert_host,
+                     alert_name,
+                     status,
+                     notify_by,
+                     ';'.join(rest), fn])
+        data.write(payload + '\n')
+        line_count=line_count+1
+    data.close()
+    if line_count>0:
+      delete_query="DELETE FROM nagios_log_data WHERE filename='" + fn + "'; COMMIT;"
+      print delete_query
+      query_vertica(delete_query)
+      print "delete successful"
+      print "importing", line_count , "lines into Vertica"
+      load_into_vertica(transform_output_file, 'nagios_log_data', delimiter=chr(1), field_order=field_order)
+      print "import successful"
+      cnxn.commit()
 
-    logger.debug('loading data into Vertica')
-    field_order = ['tot_requests_on_date', '_year_quarter', 'bl_date',
-                   'product', 'v_prod_major', 'prod_os', 'v_prod_os',
-                   'channel', 'locale','continent_code', 'cntry_code',
-                   'distro_name', 'distro_version', 'buildid']
-    load_into_vertica(input_file,
-                           vertica_table_name,
-                           field_order = field_order)
-
-
-    # verify
-    logger.debug('verifying that data was loaded successfully')
-    sql_tmpl = ("SELECT count(1) as c FROM {tbl_name} WHERE bl_date >= '{start_date}'"
-                " AND bl_date <= '{end_date}';")
-
-    sql = sql_tmpl.format(tbl_name = vertica_table_name,
-                          start_date = start_date.strftime("%Y-%m-%d"),
-                          end_date = end_date.strftime("%Y-%m-%d"))
-
-    cnt = sum(map(int, (i[0] for i in query_vertica(sql))))
-
-    if cnt != meta['row_count']:
-      logger.error('count from vertica %d does not match count from transform %d',
-                   cnt, meta['row_count'])
-      raise IOError, 'verification failed'
-
-    # update last_updated with result
-    logger.debug('inserting completed load in last_updated for monitoring')
-    last_updated_sql = ("insert into last_updated values ('copy_adi_dimensional_by_date_s3', now(), '%s');" % __file__)
-    query_vertica(last_updated_sql)
-    
-    # All worked, commit it all
-    logger.debug('final transaction commit')
-    cnxn.commit()
 
 if __name__ == '__main__':
 
@@ -121,19 +124,26 @@ if __name__ == '__main__':
     logging.basicConfig(format = '%(asctime)s %(name)s:%(levelname)s: %(message)s',
                       level = logging.DEBUG)
 
-    if (len(sys.argv) == 3):
-      start_date = datetime.strptime(sys.argv[1], '%Y-%m-%d')
-      end_date = datetime.strptime(sys.argv[2], '%Y-%m-%d')
-    else:
-      now = date.today() - timedelta(days = 1)  # always get data for previous day
-      start_date = now
-      end_date = now
+  
+    parser = OptionParser()
+    parser.add_option('-f', '--file', dest='log_file', help='Log file to process', type='string')
+    (options, args) = parser.parse_args()
 
-    logger.debug('starting ETL start_date %s end_date %s',
-                 start_date.strftime('%Y-%m-%d'),
-                 end_date.strftime('%Y-%m-%d'))
+    if options.log_file is None:
+        from datetime import datetime, timedelta
+        today = datetime.today()
+        yesterday = today - timedelta(1)
+        Date = yesterday.strftime('%Y-%m-%d')
+        for file in nagios_dc:
+            fn = file + '.nagios_' + Date + '.gz'
+            fullfn = os.path.join(default_path, fn)
+            print fullfn
+            ProcessIt(fullfn,fn)
+    elif not options.log_file is None:
+        print "Manually processing options.log_file"
+        filen = os.path.basename(options.log_file)
+        ProcessIt(options.log_file,filen)
 
-    main(start_date, end_date)
     logger.debug('ETL successfully completed')
 
   except:

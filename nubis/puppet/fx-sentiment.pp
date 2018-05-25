e92578ea7b0e61794a4eac01d24b122272860a4a
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-25 09:30:03 -0400

    WIP: Add fx-sentiment code and dependencies

---
+++ nubis/puppet/fx-sentiment.pp
@@ -0,0 +1,43 @@
+yumrepo { 'google-cloud-sdk':
+  descr    => 'Google Cloud SDK',
+  baseurl  => 'https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64',
+  enabled  => 1,
+  gpgcheck => 1,
+  repo_gpgcheck => 1,
+  gpgkey   => "https://packages.cloud.google.com/yum/doc/yum-key.gpg\n       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg",
+}
+
+package { 'google-cloud-sdk':
+  ensure => latest,
+  require => [
+    Yumrepo['google-cloud-sdk'],
+  ]
+}
+
+python::pyvenv { "${virtualenv_path}/fx-sentiment" :
+  ensure  => present,
+  version => '3.4',
+  require => [
+    File[$virtualenv_path],
+  ],
+}
+
+vcsrepo { '/opt/etl/fx-sentiment':
+  ensure   => present,
+  provider => 'git',
+  source   => 'https://github.com/mozilla-it/fx-sentiment-analysis.git',
+  require  => [
+    File['/opt/etl'],
+  ],
+}
+
+# Install fx-sentiment dependencies
+python::requirements { 'fx-sentiment':
+  requirements => '/opt/etl/fx-sentiment/RequiredPackages.txt',
+  forceupdate => true,
+  virtualenv => "${virtualenv_path}/fx-sentiment",
+  require    => [
+    Vcsrepo['/opt/etl/fx-sentiment'],
+    Python::Pyvenv["${virtualenv_path}/fx-sentiment"],
+  ],
+}

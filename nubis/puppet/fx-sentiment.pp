f24ad7a6fd658f474dd9dd78051bb58cd5b72ee2
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-25 09:38:35 -0400

    puppet lint

--- nubis/puppet/fx-sentiment.pp
+++ nubis/puppet/fx-sentiment.pp
@@ -1,14 +1,14 @@
 yumrepo { 'google-cloud-sdk':
-  descr    => 'Google Cloud SDK',
-  baseurl  => 'https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64',
-  enabled  => 1,
-  gpgcheck => 1,
+  descr         => 'Google Cloud SDK',
+  baseurl       => 'https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64',
+  enabled       => 1,
+  gpgcheck      => 1,
   repo_gpgcheck => 1,
-  gpgkey   => "https://packages.cloud.google.com/yum/doc/yum-key.gpg\n       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg",
+  gpgkey        => "https://packages.cloud.google.com/yum/doc/yum-key.gpg\n       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg",
 }
 
 package { 'google-cloud-sdk':
-  ensure => latest,
+  ensure  => latest,
   require => [
     Yumrepo['google-cloud-sdk'],
   ]
@@ -34,14 +34,14 @@ vcsrepo { '/opt/etl/fx-sentiment':
 # Install fx-sentiment dependencies
 python::requirements { 'fx-sentiment':
   requirements => '/opt/etl/fx-sentiment/RequiredPackages.txt',
-  forceupdate => true,
-  virtualenv => "${virtualenv_path}/fx-sentiment",
-  require    => [
+  forceupdate  => true,
+  virtualenv   => "${virtualenv_path}/fx-sentiment",
+  require      => [
     Vcsrepo['/opt/etl/fx-sentiment'],
     Python::Pyvenv["${virtualenv_path}/fx-sentiment"],
   ],
-}->
-exec { 'install NTLK data':
-  command => "${virtualenv_path}/fx-sentiment/bin/python -m nltk.downloader -d /usr/local/share/nltk_data all",
+}
+  -> exec { 'install NTLK data':
+  command   => "${virtualenv_path}/fx-sentiment/bin/python -m nltk.downloader -d /usr/local/share/nltk_data all",
   logoutput => true,
 }

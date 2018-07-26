3394863f022bf3fb322d6b9cf7c1c88ac7d626c0
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-07-26 12:23:04 -0400

    bump root volume size to 128

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -11,7 +11,7 @@ module "worker" {
   nubis_sudo_groups = "${var.nubis_sudo_groups}"
   nubis_user_groups = "${var.nubis_user_groups}"
 
-  root_storage_size = "64"
+  root_storage_size = "128"
   instance_type     = "t2.small"
 }
 

541af1aafccc0dc9c6aa2b83c7c6559b976b041f
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-08-30 13:36:41 -0400

    add back data-collectors fixed

--- nubis/puppet/0_python.pp
+++ nubis/puppet/0_python.pp
@@ -30,32 +30,32 @@ package { 'unixODBC-devel':
 }
 
 # Install Mozilla's data-collector
-#python::pip { 'data-collectors':
-#  ensure     => 'present',
-#  virtualenv => "${virtualenv_path}/data-collectors",
-#  url        => 'git+https://github.com/gozer/data-collectors@71db14d45800775f78a1a33604235c5b37550909',
-#  require    => [
-#    Package['gcc-c++'],
-#    Package['unixODBC-devel'],
-#  ],
-#}
+python::pip { 'data-collectors':
+  ensure     => 'present',
+  virtualenv => "${virtualenv_path}/data-collectors",
+  url        => 'git+https://github.com/gozer/data-collectors@d1d5bb7df0bbcc61116374414f60848cd356f80b',
+  require    => [
+    Package['gcc-c++'],
+    Package['unixODBC-devel'],
+  ],
+}
 
-#file { '/usr/local/bin/data-collectors':
-#  ensure  => link,
-#  target  => '/usr/local/virtualenvs/data-collectors/bin/data-collectors',
-#  require => [
-#    Python::Pip['data-collectors'],
-#  ],
-#}
+file { '/usr/local/bin/data-collectors':
+  ensure  => link,
+  target  => '/usr/local/virtualenvs/data-collectors/bin/data-collectors',
+  require => [
+    Python::Pip['data-collectors'],
+  ],
+}
 
 file { '/etc/data-collectors':
   ensure => directory,
 }
 
-#file { '/usr/local/virtualenvs/data-collectors/lib/python3.4/site-packages/collectors/defaults':
-#  ensure  => link,
-#  target  => '../../../../collectors/defaults',
-#  require => [
-#    Python::Pip['data-collectors'],
-#  ],
-#}
+file { '/usr/local/virtualenvs/data-collectors/lib/python3.4/site-packages/collectors/defaults':
+  ensure  => link,
+  target  => '../../../../collectors/defaults',
+  require => [
+    Python::Pip['data-collectors'],
+  ],
+}

aa970a355315b8ff9cfedf9479b97a61ca8f3ce3
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-10-15 12:20:22 -0400

    try and our Boomi ETL code

---
+++ nubis/puppet/boomi.pp
@@ -0,0 +1,46 @@
+package { 'python36':
+  ensure => present,
+}
+
+package { 'python36-devel':
+  ensure => present,
+}
+
+python::virtualenv { "${virtualenv_path}/boomi" :
+  ensure  => present,
+  version => '3.6',
+  require => [
+    Package['python36'],
+    Package['python36-devel'],
+    File[$virtualenv_path],
+  ],
+}
+
+file { '/opt/etl/boomi':
+  ensure  => directory,
+  require => [
+    File['/opt/etl'],
+  ]
+}
+
+file { '/var/lib/etl/boomi':
+  ensure  => directory,
+  owner   => 'etl',
+  group   => 'etl',
+  mode    => '0755',
+
+  require => [
+    User['etl'],
+    Group['etl'],
+    File['/var/lib/etl'],
+  ]
+}
+
+# Install Mozilla boomi python libraries 
+python::pip { 'mozilla_etl':
+  ensure     => 'present',
+  virtualenv => "${virtualenv_path}/boomi",
+  url        => 'git+https://github.com/gozer/mozilla_etl.git',
+  require    => [
+  ],
+}

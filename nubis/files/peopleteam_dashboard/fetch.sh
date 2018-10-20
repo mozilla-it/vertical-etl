f7cad10563d4c0e01522d2d407d5699477c78c89
Chris Valaas <cvalaas@mozilla.com>
2018-10-19 20:28:53 -0700

    changing peopleteam dashboard tables and when the jobs run

--- nubis/files/peopleteam_dashboard/fetch.sh
+++ nubis/files/peopleteam_dashboard/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-/usr/local/bin/peopleteam-dashboard-fetcher --output-dir /var/lib/etl/peopleteam_dashboard/
+/usr/local/bin/peopleteam-dashboard-fetcher --output-dir /var/lib/etl/peopleteam_dashboard/ --date `date --date="1 day ago" +%Y-%m-%d`
--- nubis/files/peopleteam_dashboard_monthly/load.sh
+++ nubis/files/peopleteam_dashboard_monthly/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-/usr/local/bin/vertica-csv-loader /opt/etl/peopleteam_dashboard_monthly/load.yml
+/usr/local/bin/vertica-csv-loader --start-date `date --date="1 day ago" +%Y-%m-%d` /opt/etl/peopleteam_dashboard_monthly/load.yml

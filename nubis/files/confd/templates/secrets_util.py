6845343210be334fdd65deb1bbf8aac4606d0fa1
Chris Valaas <cvalaas@mozilla.com>
2018-11-01 13:05:41 -0700

    adding secrets/config for new Util.py module

---
+++ nubis/files/confd/templates/secrets_util.py
@@ -0,0 +1,6 @@
+config = {
+  'proxies'             : {"https" : "http://proxy.service.consul:3128"},
+  'MozGeo': {
+    'google_api_key' : '{{ getv "/config/Util/MozGeo/GoogleAPIKey" "mozgeo-google-api-key-not-set" }}',
+  }
+}

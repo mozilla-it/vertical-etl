9c2853a6b2aed13c8634b0ceaaa5dc557dee063a
Chris Valaas <cvalaas@mozilla.com>
2018-11-05 10:07:16 -0800

    new data-integrations

--- nubis/puppet/salesforce_sfmc.pp
+++ nubis/puppet/salesforce_sfmc.pp
@@ -16,7 +16,7 @@ python::pyvenv { "${virtualenv_path}/data-integrations" :
 python::pip { 'data-integrations':
   ensure     => 'present',
   virtualenv => "${virtualenv_path}/data-integrations",
-  url        => 'git+https://github.com/mozilla-it/data-integrations@8fcfe2b3af71fb7651e9b8e4bc94bcb08a885948',
+  url        => 'git+https://github.com/mozilla-it/data-integrations@58a0ee10b6c4190c85d3e868de26a1e5e6c1289e',
   require    => [
   ],
 }

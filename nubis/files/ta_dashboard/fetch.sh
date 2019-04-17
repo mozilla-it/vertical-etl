4932002655866b53698fa95a60e476bda05e5984
Chris Valaas <cvalaas@mozilla.com>
2019-04-17 12:36:33 -0700

    switching date to current day

--- nubis/files/ta_dashboard/fetch.sh
+++ nubis/files/ta_dashboard/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-/usr/local/bin/peopleteam-dashboard-fetcher --output-dir /var/lib/etl/ta_dashboard/ --ta-dashboard --date `date --date="1 day ago" +%Y-%m-%d`
+/usr/local/bin/peopleteam-dashboard-fetcher --output-dir /var/lib/etl/ta_dashboard/ --ta-dashboard --date `date +%Y-%m-%d`
--- nubis/files/ta_dashboard/load.sh
+++ nubis/files/ta_dashboard/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-/usr/local/bin/vertica-csv-loader --start-date `date --date="1 day ago" +%Y-%m-%d` /opt/etl/ta_dashboard/load.yml
+/usr/local/bin/vertica-csv-loader --start-date `date +%Y-%m-%d` /opt/etl/ta_dashboard/load.yml
